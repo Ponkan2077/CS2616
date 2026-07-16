1e51bf07fe34f701f7db394046b6c3fa16259b73
Rey Ursus Acuña Flores <138117500+Ponkan2077@users.noreply.github.com>
2026-07-16 19:28:08 +0800

    Add DiseaseClass admin registration
    Registered DiseaseClass model with custom admin options.

--- farmmap/admin.py
+++ farmmap/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import DiseaseClass
 
-# Register your models here.
+
+@admin.register(DiseaseClass)
+class DiseaseClassAdmin(admin.ModelAdmin):
+    list_display = ("name", "display_order", "color_hex", "danger_rank", "is_healthy")
+    list_editable = ("display_order", "color_hex", "danger_rank", "is_healthy")
+    fields = (
+        "name", "description", "display_order", "color_hex", "marker_key", "danger_rank", "is_healthy",
+        "recommendation_mild", "recommendation_moderate", "recommendation_severe",
+    )
