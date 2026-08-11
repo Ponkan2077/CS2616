53dd15f551b7e09cba4c59e6da239da67d0634bc
Rey Ursus Acuña Flores <138117500+Ponkan2077@users.noreply.github.com>
2026-08-11 19:22:49 +0800

    Add service worker path to detection URLs

--- farmmap/urls.py
+++ farmmap/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('map/', views.farm_map, name='farm_map'),
     path('map/marker/<str:tree_id>/', views.tree_marker_detail, name='tree_marker_detail'),
     path('detection/', views.disease_detection, name='disease_detection'),
+    path('detection/sw.js', views.service_worker_js, name='service_worker_js'),
     path('detection/analyze/', views.analyze_detection, name='analyze_detection'),
     path('detection/upload-url/', views.request_upload_url, name='request_upload_url'),
     path('detection/save/', views.save_detection, name='save_detection'),
